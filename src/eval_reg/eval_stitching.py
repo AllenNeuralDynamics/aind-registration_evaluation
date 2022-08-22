3e6322c86121f716caccdd21856512d7f5e8c524
sharmishtaa <shtaaa@gmail.com>
2022-08-22 16:39:22 -0700

    First Commit

---
+++ src/eval_reg/eval_stitching.py
@@ -0,0 +1,27 @@
+""" Module to evaluate stitching of large scale data.
+"""
+
+import logging
+
+import pandas as pd
+
+
+class EvalStitching:
+    """
+    Class to Evaluate Stitching.
+    """
+    def __init__(self):
+        """
+        Args:
+            filename (str): Input Filename.
+        """
+        self.msg = msg
+
+    def evaluate():
+	# Read Input Files 
+	# Read transforms
+	# Find Overlaps
+	# Select points/feature detection
+	# Calculate features
+	# Compute Distance Metric
+	# Calculate statistics
