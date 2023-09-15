updating visualization for 3D, fixing 3d output of histogram of gradients and generalizing some function to 3D

src/aind_registration_evaluation/main_qa.py
@@ -5,7 +5,6 @@ of large scale data.
 import logging
 import os
 from pathlib import Path
-from typing import Union
 
 import numpy as np
 import yaml
@@ -13,16 +12,15 @@ from argschema import ArgSchemaParser
 from scipy.ndimage import gaussian_filter
 
 from aind_registration_evaluation import sample, util
-from aind_registration_evaluation._shared.types import ArrayLike
+from aind_registration_evaluation._shared.types import ArrayLike, PathLike
 from aind_registration_evaluation.io import extract_data, get_data
 from aind_registration_evaluation.metric import (
     ImageMetricsFactory, compute_feature_space_distances,
     get_pairs_from_distances)
 from aind_registration_evaluation.params import EvalRegSchema
 from aind_registration_evaluation.sample import *
-
-# IO types
-PathLike = Union[str, Path]
+from aind_registration_evaluation.util.intersection import \
+    generate_overlap_slices
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -37,39 +35,6 @@ logging.disable("DEBUG")
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
-
-
-def generate_overlap_slices(
-    shapes: List[Tuple], orientation: str, overlap_ratio: float
-):
-    image_1_shape = shapes[0]
-    image_2_shape = shapes[1]
-    # Getting overlapping area for images
-    overlap_area_1 = (np.array(image_1_shape) * overlap_ratio).astype(int)
-    overlap_area_2 = (np.array(image_2_shape) * overlap_ratio).astype(int)
-
-    if orientation == "x":
-        # Left
-        offset_img_1 = image_1_shape[1] - overlap_area_1[1]
-
-        slices_1 = (
-            slice(0, image_1_shape[0]),
-            slice(offset_img_1, image_1_shape[1]),
-        )
-        # Right
-        slices_2 = (slice(0, image_2_shape[0]), slice(0, overlap_area_2[1]))
-
-    else:
-        # Top
-        offset_img_1 = image_1_shape[0] - overlap_area_1[0]
-        slices_1 = (
-            slice(offset_img_1, image_1_shape[0]),
-            slice(0, image_1_shape[1]),
-        )
-        # Bottom
-        slices_2 = (slice(0, overlap_area_2[0]), slice(0, image_2_shape[1]))
-
-    return slices_1, slices_2, offset_img_1
 
 
 def generate_feature_decriptors(
@@ -177,8 +142,8 @@ def generate_feature_decriptors(
             image_gradient_orientation=gradient_orientations,
             keypoint=keypoint,
             n_dims=img.ndim,
-            window_size=16,
-            bins=[8],
+            # window_size=16,
+            # bins=[8],
         )
         for keypoint in img_keypoints
     ]
@@ -188,6 +153,12 @@ def generate_feature_decriptors(
     for key_feat in img_keypoints_features:
         keypoints.append(key_feat["keypoint"])
         features.append(key_feat["feature_vector"])
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(img_response[10, ...])
+    # plt.show()
+
+    print("keypoint - features: ", len(keypoints), len(features))
 
     return {
         "keypoints": np.array(keypoints),
@@ -356,7 +327,7 @@ class EvalStitching(ArgSchemaParser):
                     metric_name,
                 )
 
-    def run_misalignment_2d(
+    def run_misalignment(
         self,
         n_keypoints: int,
         pad_width: int,
@@ -434,18 +405,19 @@ class EvalStitching(ArgSchemaParser):
         )
 
         # Compute keypoints between images
-
         slices_1, slices_2, offset_img_1 = generate_overlap_slices(
             shapes=[image_1_shape, image_2_shape],
             orientation=orientation,
             overlap_ratio=overlap_ratio,
         )
 
+        LOGGER.info("Getting keypoints and feature decriptors for image 1.")
         img_1_dict = generate_feature_decriptors(
             image_1_data[slices_1],
             n_keypoints=n_keypoints,
             pad_width=pad_width,
         )
+        LOGGER.info("Getting keypoints and feature decriptors for image 2.")
         img_2_dict = generate_feature_decriptors(
             image_2_data[slices_2],
             n_keypoints=n_keypoints,
@@ -464,6 +436,7 @@ class EvalStitching(ArgSchemaParser):
             right_image_keypoints,
         )
 
+        LOGGER.info("Computing pair-wise feature space distances")
         distances = compute_feature_space_distances(
             feature_vector_img_1, feature_vector_img_2, feature_weight=0.2
         )
@@ -533,8 +506,9 @@ class EvalStitching(ArgSchemaParser):
             & (point_distances <= mean + threshold)
         )
 
-        print(f"\n[!] Median euclidean distance in pixels/voxels: {median}")
-        print(f"[!] Mean euclidean distance in pixels/voxels: {mean}")
+        unit = "pixels" if image_1_data.ndim == 2 else "voxels"
+        LOGGER.info(f"\n[!] Median euclidean distance in {unit}: {median}")
+        LOGGER.info(f"[!] Mean euclidean distance in {unit}: {mean}")
 
         if self.args["visualize"]:
             util.visualize_misalignment_images(

src/aind_registration_evaluation/sample/sample_points.py
@@ -584,7 +584,11 @@ def kd_compute_keypoints_hog(
                     cell_orientation_z_xy = region_orientation_z_yx[quadrant]
                     _range = ((-180, 180), (-90, 90))
 
-                    hist, hist_bins = np.histogram2d(
+                    (
+                        hist,
+                        hist_bins_polar,
+                        hist_bins_elevation,
+                    ) = np.histogram2d(
                         cell_orientation_z_xy.flatten(),
                         cell_orientation_yx.flatten(),
                         bins=bins,
@@ -594,7 +598,12 @@ def kd_compute_keypoints_hog(
                     )
 
                     # Recomputing angles since histogram returns bins + 1
-                    hist_bins = (hist_bins[:-1] + hist_bins[1:]) / 2.0
+                    hist_bins_polar = (
+                        hist_bins_polar[:-1] + hist_bins_polar[1:]
+                    ) / 2.0
+                    hist_bins_elevation = (
+                        hist_bins_elevation[:-1] + hist_bins_elevation[1:]
+                    ) / 2.0
                     weighted_gradient_hist.append(hist)
 
     feature_vector = np.array(weighted_gradient_hist)

src/aind_registration_evaluation/util/visualization.py
@@ -717,7 +717,79 @@ def visualize_misalignment_images(
         plt.show()
 
     else:
-        pass
+        # return
+        # 3D image, find a way to render 3D images with 3D grid
+
+        size_x = max(bounds_1[1][0], bounds_2[1][0])
+        size_y = max(bounds_1[1][1], bounds_2[1][1])
+        size_z = max(bounds_1[1][2], bounds_2[1][2])
+
+        # Image within same coordinate system
+        adjusted_img_1 = np.ones((size_x, size_y, size_z)) * 255
+        adjusted_img_2 = np.ones((size_x, size_y, size_z)) * 255
+
+        # Getting data from the images to common coordinate image
+        adjusted_img_1[
+            : bounds_1[1][0], : bounds_1[1][1], : bounds_1[1][2]
+        ] = image_1_data
+        adjusted_img_2[
+            bounds_2[0][0] :, bounds_2[0][1] :, bounds_2[0][2] :
+        ] = image_2_data
+
+        # Setting Z, Y, X positions of points within the grid
+        z_points_img_1 = [point[0] for point in keypoints_img_1]
+        y_points_img_1 = [point[1] for point in keypoints_img_1]
+        x_points_img_1 = [point[2] for point in keypoints_img_1]
+
+        # Setting Z,Y,X positions of points within the grid
+        # that were used to metric estimation
+        z_points_img_2 = [point[0] for point in keypoints_img_2]
+        y_points_img_2 = [point[1] for point in keypoints_img_2]
+        x_points_img_2 = [point[2] for point in keypoints_img_2]
+
+        points_img_1 = [z_points_img_1, y_points_img_1, x_points_img_1]
+        points_img_2 = [z_points_img_2, y_points_img_2, x_points_img_2]
+
+        # visualize_image_3D(combined_volume, points, selected_points)
+
+        # Rectangles to divide images
+        rectangle_image_1 = Rectangle(
+            xy=(bounds_1[0][2], bounds_1[0][1]),
+            width=bounds_1[1][2],
+            height=bounds_1[1][1],
+            linewidth=1,
+            edgecolor="#FF2D00",
+            facecolor="none",
+            linestyle=":",
+        )
+
+        rectangle_image_2 = Rectangle(
+            xy=(bounds_2[0][2], bounds_2[0][1]),
+            width=bounds_1[1][2],
+            height=bounds_1[1][1],
+            linewidth=1,
+            edgecolor="#13FF00",
+            facecolor="none",
+            linestyle="--",
+        )
+
+        fig, ax = plt.subplots(nrows=1, ncols=1)
+        fig.suptitle(f"Metric: {metric_name}", fontsize=16)
+
+        tracker = SliceTracker(
+            fig_axes=ax,
+            image_1_data=adjusted_img_1,
+            image_2_data=adjusted_img_2,
+            points=points_img_1,
+            selected_ponts=points_img_2,
+            # rectangles=[rectangle_image_1, rectangle_image_2]
+        )
+
+        ax.add_patch(rectangle_image_1)
+        ax.add_patch(rectangle_image_2)
+        fig.canvas.mpl_connect("scroll_event", tracker.on_scroll)
+        fig.tight_layout()
+        plt.show()
 
 
 def plot_matches(